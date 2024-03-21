ceac7ad01e8153bab2c1ddf4af03282404347784
Andrew Denegar <adenegar@carroll.edu>
2024-03-21 16:31:48 -0600

    Documentation and testing for ChunkManager and Player

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,8 @@
  * Player.java
  * Authors: Andrew Denegar, Nick Clouse, Molly O'Connor
  * Date: 2024-03-20
- * Description: Player.java 
+ * Description: Player.java holds character attributes including images, state (Idle, Move...), facing (N, NE, E...) 
+ * 	and methods including loading images, updating state, and drawing. 
  */
 
 package src;
@@ -20,31 +21,53 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 
 public class Player implements GameVariables {
-
+	
 	private static final String FILE_LOCATION = "images/";
-
-	private int drawCount = 0;
-
-	public State currentState = State.Idle;
-	public Facing currentFacing = Facing.N;
-
-	public Map<State, Map<Facing, List<BufferedImage>>> images = new HashMap<>();
-
+	
 	public enum State {
 		Idle, Move
 	}
 
+	// The direction the player is currently facing.
 	public enum Facing {
 		S, SE, E, NE, N, NW, W, SW
 	}
 
+	// draw count is used
+	private int drawCount = 0;
+
+	// Set initial player attributes
+	private State currentState = State.Idle;
+	private Facing currentFacing = Facing.N;
+
+	// A map of images that can be accessed by first specifying the player state and direction faced
+	private Map<State, Map<Facing, List<BufferedImage>>> images = new HashMap<>();
+	
+	/**
+	 * Load images for each player state
+	 * @param character_name: the name of the player file to be selected (Civilian1, Civilian2, Civilian1(black), etc)
+	 */
 	public void load_images(String character_name) {
-		load_spritesheet(character_name, State.Idle, 4, 2);
-		load_spritesheet(character_name, State.Move, 4, 8);
+		// Declare spritesheet dimensions
+		final int SPRITESHEET_WIDTH = 4;
+		final int SPRITESHEET_IDLE_HEIGHT = 2;
+		final int SPRITESHEET_MOVE_HEIGHT = 8;
+		
+		// Load a spritesheet for each player state
+		load_spritesheet(character_name, State.Idle, SPRITESHEET_WIDTH, SPRITESHEET_IDLE_HEIGHT);
+		load_spritesheet(character_name, State.Move, SPRITESHEET_WIDTH, SPRITESHEET_MOVE_HEIGHT);
 	}
 
-	public void load_spritesheet(String character_name, State playerState, int xDim, int yDim) {
+	/**
+	 * Load an individual spritesheet
+	 * @param character_name: the name of the player file to be selected (Civilian1, Civilian2, Civilian1(black), etc)
+	 * @param playerState: The player state that is to be loaded
+	 * @param xDim: The x dimension of the spritesheet
+	 * @param yDim: The y dimension of the spritesheet
+	 */
+	private void load_spritesheet(String character_name, State playerState, int xDim, int yDim) {
 		BufferedImage spriteSheet = null;
+		// Load the spritesheet file
 		if (playerState.toString() != null) {
 			final String resource = FILE_LOCATION + character_name + "_" + playerState.toString() + ".png";
 			try {
@@ -55,15 +78,23 @@ public class Player implements GameVariables {
 		}
 		// Split the spritesheet into individual images.
 		// TODO: The image reading process could be improved in the future:
-		// 1. Reuse variables from one resize to the next OR
-		// 2. Resize images originally, then you won't have to resize them here
+		// - Resize images originally, then you won't have to resize them here
 		if (spriteSheet != null) {
+			// Create a new HashMap for the specified player state.
 			images.put(playerState, new HashMap<>());
-			int height = spriteSheet.getHeight();
-			int width = spriteSheet.getWidth();
-			int framesPerAnim = xDim * yDim / Facing.values().length;
+			// Save constants used for spritesheet loading
+			final int height = spriteSheet.getHeight();
+			final int width = spriteSheet.getWidth();
+			final int framesPerAnim = xDim * yDim / Facing.values().length;
+			// Save image resizing constants
+			final double WIDTH_POSITION_ADJUSTMENT = 2.5;
+			final double HEIGHT_POSITION_ADJUSTMENT = 3.2;
+			final double WIDTH_SIZE_ADJUSTMENT = 4;
+			final double HEIGHT_SIZE_ADJUSTMENT = 2.7;
+			// Count and direction will be changed based on the number of the image being loaded.
 			int count = 0;
 			Facing direction = Facing.values()[count];
+			// Create the first list for the original state-facing pair.
 			images.get(playerState).put(Facing.S, new LinkedList<BufferedImage>());
 			for (int y = 0; y < yDim; y++) {
 				for (int x = 0; x < xDim; x++) {
@@ -73,8 +104,8 @@ public class Player implements GameVariables {
 							height / yDim);
 					// Now that we have the image split from the other part, let's remove the
 					// whitespace
-					BufferedImage finalImage = subImage.getSubimage(width / xDim / 3, height / yDim / 3,
-							width / xDim / 3, height / yDim / 3);
+					BufferedImage finalImage = subImage.getSubimage((int) (width / xDim / WIDTH_POSITION_ADJUSTMENT), (int) (height / yDim / HEIGHT_POSITION_ADJUSTMENT),
+							(int) (width / xDim / WIDTH_SIZE_ADJUSTMENT), (int) (height / yDim / HEIGHT_SIZE_ADJUSTMENT));
 
 					images.get(playerState).get(direction).add(finalImage);
 
@@ -138,20 +169,19 @@ public class Player implements GameVariables {
 		currentState = playerState;
 	}
 
-	// Draw our player
+	/**
+	 * Draw our player. Draw handles the switching from one image in a sequence to the next. 
+	 * @param g
+	 */
 	public void draw(Graphics2D g) {
 		if (drawCount < 5) { // For x ticks of the game loop, draw the same image.
 			g.drawImage(images.get(currentState).get(currentFacing).get(0), PLAYER_X, PLAYER_Y, PLAYER_WIDTH,
 					PLAYER_HEIGHT, null);
-			g.setColor(Color.RED);
-			g.drawRect(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT);
 			drawCount++;
 		} else { // Then, switch the image to the next one in the sequence.
 			BufferedImage img = images.get(currentState).get(currentFacing).remove(0);
 			g.drawImage(img, PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT, null);
 			images.get(currentState).get(currentFacing).add(img);
-			g.setColor(Color.RED);
-			g.drawRect(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT);
 			drawCount = 0;
 		}
 
@@ -201,10 +231,42 @@ public class Player implements GameVariables {
 		// Choose the player model analyzed
 		p1.load_images("Civilian1(black)");
 
+		// Start: Unit Testing
+		boolean all_passed = true;
+		// Update the state as if all keys were pressed at the same time
+		// By default, this should set the current direction faced to North
+		p1.updateState(true, true, true, true);
+		if (p1.currentFacing != Facing.N) {
+			System.out.println("When updateState() registers all keys being pressed, Facing should be set to north.");
+			all_passed = false;
+		}
+		// Now set key press
+		p1.updateState(false, false, true, false);
+		if (p1.currentFacing != Facing.E) {
+			System.out.println("Key press of right should change the player to be facing east.");
+			all_passed = false;
+		}
+		p1.updateState(false, false, false, false);
+		if (p1.currentState != State.Idle) {
+			System.out.println("When no key is pressed, the current state should be idle.");
+			all_passed = false;
+		}
+		p1.setFacing(Facing.SE);
+		if (p1.currentFacing != Facing.SE) {
+			System.out.println("setFacing() failed to set the direction of the player to southeast.");
+			all_passed = false;
+		}
+		if (all_passed) {
+			System.out.println("All cases passed, good job.");
+		} else {
+			System.out.println("At least one case failed");
+		}
+				
+		// Start: Image testing
 		initializeGUI();
-
+		
 		// Change these two variables to modify the animations tested
-		State playerState = State.Idle; // Test the player state images (Move, Idle, etc.)
+		State playerState = State.Move; // Test the player state images (Move, Idle, etc.)
 		Facing direction = Facing.S; // Test the direction the player is facing
 		int speed = (int) (0.1 * 1000); // Set seconds (first number) between each image.
 
