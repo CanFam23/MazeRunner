d0b5c3933d686976ab05edb550a896280d8b930b
CanFam23 <nickclouse03@gmail.com>
2024-04-08 11:33:10 -0600

    fixed imports

--- src/panels/GamePanel.java
+++ src/panels/GamePanel.java
@@ -295,7 +295,7 @@ public class GamePanel extends JPanel implements Runnable, GameVariables {
 			if (ourPlayer.getState() != "Attack") {
 				// Set our player to be attacking
 				ourPlayer.setState(sprites.Player.State.Attack);
-				ourPlayer.attack();
+				ourPlayer.attacking();
 				ourPlayer.resetDrawCount();
 			}
 			ourPlayer.lockState();
