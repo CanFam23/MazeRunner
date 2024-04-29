ddee888690383b0e92b19b9612305dfd02672dbe
CanFam23 <nickclouse03@gmail.com>
2024-04-28 23:07:52 -0600

    unused code

--- src/chunks/ChunkManager.java
+++ src/chunks/ChunkManager.java
@@ -562,7 +562,6 @@ public class ChunkManager implements GameVariables {
 	 * @return true If level was loaded correctly.
 	 */
 	public boolean loadLevel(int levelNum, int levelVersionNumber) {
-//		levelNum = 0;
 		levelName = "level_" + levelNum + "_v" + levelVersionNumber;
 		if (levelNum == 0) {
 			levelName = "level_0";
